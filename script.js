29398bedcf334dc2eff477cb25e95ee7b2445d6f
Mateus <mateus.saraiva09@gmail.com>
2022-11-11 01:23:08 -0300

    Update javaScript

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
+
+//No inicio foi feito uma função para fazer a conversão
+
+
 const convertValue = (value) => {
 
-    const valueOne = 1
+    const valueOne = 1      //para testes os valores foram fixos
     const valueTwo = 10
 
     const result = value*valueOne/valueTwo
