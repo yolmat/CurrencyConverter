d8a6631d0e6f832e6dc7af1a409f116e1603023a
Mateus <mateus.saraiva09@gmail.com>
2022-11-11 01:22:03 -0300

    Creat javaScript

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-console.log("hello word")
+const convertValue = (value) => {
+
+    const valueOne = 1
+    const valueTwo = 10
+
+    const result = value*valueOne/valueTwo
+
+    return(result)
+
+}
+
+console.log(convertValue(1))
