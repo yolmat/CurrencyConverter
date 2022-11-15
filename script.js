4771fff60afe70d3b37a601b5a7ff566b24cc539
Mateus <mateus.saraiva09@gmail.com>
2022-11-15 03:43:09 -0300

    javascript

--- script.js
+++ script.js
@@ -1,16 +1,133 @@
+// Buscando o button
+const buttonConverter = document.getElementById("converter");
 
-//No inicio foi feito uma função para fazer a conversão
+// Função para converter
+const converter = () => {
+    // Buscando elementos necessarios
+    const input = document.getElementById("value").value;
+    const selectFrom = document.getElementById("currencySelectFrom");
+    const selectTo = document.getElementById("currencySelectTo");
 
+    // Adicionando let's com os valores que seram convertidos
+    const valueFrom = 1;
+    let valueTo = 0;
 
-const convertValue = (value) => {
+    // Buscando saber quem vai ser chamado
+    let optionFrom = selectFrom.value;
+    let optionTo = selectTo.value;
 
-    const valueOne = 1      //para testes os valores foram fixos
-    const valueTwo = 10
+    const real = "R$ Real";
+    const dolar = "US$ Dolar";
+    const euro = "€ Euro";
+    const bitCoin = "₿ BitCoin";
+    const ethereum = "Ξ Ethereum";
 
-    const result = value*valueOne/valueTwo
+    // Procurando as converções
 
-    return(result)
+    // Conversão do real
+    if (optionFrom === real && optionTo === dolar) {
+        valueTo = 5; //dolar
+    } 
+    
+    else if (optionFrom === real && optionTo === real) {
+        valueTo = 1; //real
+    } 
+    
+    else if (optionFrom === real && optionTo === euro) {
+        valueTo = 4; //euro
+    } 
+    
+    else if (optionFrom === real && optionTo === bitCoin) {
+        valueTo = 10000; //bitCoin
+    } 
+    
+    else if (optionFrom === real && optionTo === ethereum) {
+        valueTo = 5000; //ethereum
+    }
+    // conversão do dolar
+    
+    else if (optionFrom === dolar && optionTo === dolar) {
+        valueTo = 1; //dolar
+    } 
+    
+    else if (optionFrom === dolar && optionTo === real) {
+        valueTo = 0.5; //real
+    } 
+    
+    else if (optionFrom === dolar && optionTo === euro) {
+        valueTo = 1.5; //euro
+    } 
+    
+    else if (optionFrom === dolar && optionTo === bitCoin) {
+        valueTo = 10000; //bitCoin
+    } 
+    
+    else if (optionFrom === dolar && optionTo === ethereum) {
+        valueTo = 5000; //ethereum
+    }
+    // conversão do euro
+    
+    else if (optionFrom === euro && optionTo === dolar) {
+        valueTo = 1; //dolar
+    } 
+    
+    else if (optionFrom === euro && optionTo === real) {
+        valueTo = 0.5; //real
+    } 
+    
+    else if (optionFrom === euro && optionTo === euro) {
+        valueTo = 1.5; //euro
+    } 
+    
+    else if (optionFrom === euro && optionTo === bitCoin) {
+        valueTo = 10000; //bitCoin
+    } 
+    
+    else if (optionFrom === euro && optionTo === ethereum) {
+        valueTo = 5000; //ethereum
+    }
+    // conversão do bitCoin
+    
+    else if (optionFrom === bitCoin && optionTo === dolar) {
+        valueTo = 1; //dolar
+    } 
+    
+    else if (optionFrom === bitCoin && optionTo === real) {
+        valueTo = 0.5; //real
+    } 
+    
+    else if (optionFrom === bitCoin && optionTo === euro) {
+        valueTo = 1.5; //euro
+    } 
+    
+    else if (optionFrom === bitCoin && optionTo === bitCoin) {
+        valueTo = 10000; //bitCoin
+    } 
+    
+    else if (optionFrom === bitCoin && optionTo === ethereum) {
+        valueTo = 5000; //ethereum
+    }
+    // conversão do ethereum
+    
+    else if (optionFrom === ethereum && optionTo === dolar) {
+        valueTo = 1; //dolar
+    } 
+    
+    else if (optionFrom === ethereum && optionTo === real) {
+        valueTo = 0.5; //real
+    } 
+    
+    else if (optionFrom === ethereum && optionTo === euro) {
+        valueTo = 1.5; //euro
+    } 
+    
+    else if (optionFrom === ethereum && optionTo === bitCoin) {
+        valueTo = 10000; //bitCoin
+    } 
+    
+    else if (optionFrom === ethereum && optionTo === ethereum) {
+        valueTo = 5000; //ethereum
+    }
+};
 
-}
-
-console.log(convertValue(1))
+buttonConverter.addEventListener("click", converter)
